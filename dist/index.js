cdb1cc538f49afb5ce9d94ef9bcca86849c3c85b
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2016-12-20 16:30:23 -0500

    react components

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,14 @@
-"use strict";
+'use strict';
 
-// var Header = require('./components/Header.jsx');
+var Header = require('./components/Header.jsx');
 
-// var SharedReactComponents = {
-//   Header: Header
-// }
-
-// module.exports = SharedReactComponents;
-
-module.exports = function (_ref) {
-  var _ref$percent = _ref.percent,
-      percent = _ref$percent === undefined ? 100 : _ref$percent,
-      amount = _ref.amount;
-
-  var percentOff = percent / 100 * amount;
-  return percentOff;
+var SharedReactComponents = {
+  Header: Header
 };
+
+module.exports = SharedReactComponents;
+
+// module.exports = function({percent = 100, amount}) {  
+//   const percentOff = (percent / 100) * amount;
+//   return percentOff;
+// }
