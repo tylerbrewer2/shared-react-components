2a4e7a45bed44c240daea603171b0b943a3a29e5
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2016-12-20 12:48:57 -0500

    working

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-exports = {
-  test: function() {
-    console.log("This is a message from the demo package");
-  }
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
 }
