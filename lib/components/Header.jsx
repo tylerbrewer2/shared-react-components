882db17deb1c8fd8ec5048587118ab1236065d13
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2016-12-20 13:08:07 -0500

    test

---
+++ lib/components/Header.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Header = React.createClass({
+    render: function() {
+        return (
+            <header>
+                <h1>This is the header section</h1>
+            </header>
+        );
+    }
+});
